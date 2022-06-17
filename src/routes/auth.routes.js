36169d86758c022bb70e45897a81e28357a56b58
Carlos Boyzo <carlosboyzo@MacBook-Pro-de-Carlos.local>
2022-06-17 01:28:40 -0500

    ajuste

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const authCtrl = require('../controllers/auth.controller');
 
 router.use((req, res, next) => {
-    res.header(
-        'Access-Control-Allow-Headers',
-        'x-access-token, Origin, Content-Type, Accept'
-    );
-    next();
+  res.header(
+    'Access-Control-Allow-Headers',
+    'x-access-token, Origin, Content-Type, Accept'
+  );
+  next();
 });
 
 router.post('/signup', authCtrl.signUp);
